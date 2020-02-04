bc6ccb9fa8ed98796644ee8149c5b62e65ad69ac
msievertLTU <60272260+msievertLTU@users.noreply.github.com>
2020-02-03 20:07:48 -0500

    Initial commit

---
+++ Assets/Scripts/SceneScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneScript : MonoBehaviour
+{
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
